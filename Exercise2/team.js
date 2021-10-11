95f64ea94f66536d5745b8c7ee6100990a2d6fbe
attilatolnai <attila.tolnai@student.ehb.be>
2021-10-11 16:22:47 +0200

    Changed team.js
    made describe work when clicking on a pokemon to add it to the team

--- Exercise2/team.js
+++ Exercise2/team.js
@@ -1,12 +1,35 @@
 class Team {
-    constructor(teamname,trainer,roster){
-        this.teamname = teamname;
-        this.trainer = trainer;
-        this.roster = roster;
+    constructor(){
+        this.teamName = "Squadron";
+        this.trainer = "Attila";
+        this.roster = [];
     }
     
     describe(){
-        return `This is team ${this.teamname}, the trainer is ${this.trainer} and the team consists of ${[...this.roster]}`
+        let names = [];
+        this.roster.forEach(ele => names.push(ele.name));
+
+        return `This is team ${this.teamName}, the trainer is ${this.trainer}
+         and the team consists of ${[...names]}`
+    }
+
+    addPokemon(p){
+        let message = {
+            value: '',
+            type: 'ERROR'
+        };
+
+        if(this.roster.length == 6){
+            message.value = 'The team is full!';
+            return message;
+        }
+        if(this.roster.find(ele => ele.id == p.id)){
+            message.value = 'This pokemon is already part of your team!';
+        }
+        this.roster.push(p);
+        message.value= `The pokemon ${p.name} has been succesfully added to the team!`;
+        message.type = 'SUCCES';
+        return message;
     }
 }
 //let team = new Team("example teamname","example trainer","example rooster");
