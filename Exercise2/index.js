c2f3c028e1c6a8fe11006b2abf1e6f21ae61d587
attilatolnai <attila.tolnai@student.ehb.be>
2021-10-11 16:18:29 +0200

    Changed Teams.js
    Looped over the list of pokemon to get their values, every pokemon name, sprite and id gets displayed in the html, button  to add the pokemon to the team, refresh function to refresh the team and alerts when adding pokemon to the team

--- Exercise2/index.js
+++ Exercise2/index.js
@@ -1,38 +1,84 @@
 "use strict"
 
 import Team from "./team.js";
-let team = new Team("example teamname","example trainer","example rooster");
 
 console.log("Gotta catch em all");
 
 let list = [];
-let pokemons = [];
+let pokemon = [];
+
+let team1 = new Team();
+
+function getData(){
 fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
   .then(response => response.json())
   .then(data => {
     //console.log(data.results)
     list = data.results;
-    list.forEach(poke => {
-        fetch(poke.url)
-        .then(response => response.json())
-        .then(pokeData => {
-            pokemons.push(pokeData);
-        });
-    });
+    //For loop => loop over the list to get the data for every pokemon
+    for(let element of list){
+      fetch(element.url).then(response => {
+        return response.json();
+      }).then(data => {
+        pokemon.push(data);
+      })
+    }
   });
+}
 
   window.onload = function(){
-      setTimeout(buildList, 3000);
+
+      getData();
+      setTimeout(buildList, 1500);
 
       function buildList(){
-          console.log(pokemons);
-          pokemons.forEach(mons => {
-            //OK console.log(mons.name)
-            //OK console.log(mons.sprites.front_default)
-            //OK console.log(mons.id)
-            
-            //NOT OK console.log(mons.type)
-          })
+          console.log(pokemon);
+          let html = '';
+          pokemon.sort(function(a,b){
+            return a.id - b.id;
+          });
+
+          for(let p of pokemon){
+            html += `<div class="card">
+            <img class="card-img-top" src="${p.sprites.front_default}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">${p.name}</h5>
+            <p class="card-text">types TBD</p>
+            <a href="#" id="${p.id}" class="btn btn-primary ">Add to team</a>
+            </div>
+            </div>`
+          }
+          document.getElementById('list').innerHTML = html;
+
+          document.querySelectorAll('.btn').forEach(item => {
+            item.addEventListener('click', event => {
+                let id = event.target.id;
+                let p = pokemon.find(ele => ele.id == id);
+                let message = team1.addPokemon(p);
+                refreshTeam(message);
+            });
+        });
       }
+      refreshTeam();
+  }
+
+  function refreshTeam(m){
+    document.getElementById('team').innerHTML = team1.describe();
+
+    if(m){
+      let alertbox = document.createElement('div');
+      alertbox.classList.add('alert');
+      alertbox.setAttribute('role', 'alert');
+
+      if(m.type == 'SUCCES'){
+        alertbox.classList.add('alert-succes');
+      }else{
+        alertbox.classList.add('alert-danger');
+      }
+      alertbox.innerText = m.value;
+
+      document.getElementById('messages').innerHTML = '';
+      document.getElementById('messages').appendChild(alertbox);
+    }
   }
 
