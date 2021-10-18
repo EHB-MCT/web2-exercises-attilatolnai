a9538971f48ec148a0a546dbf53fe050dee19d2a
attilatolnai <attila.tolnai@student.ehb.be>
2021-10-18 08:47:24 +0200

    Map integrated
    A map is now integrated in the website

--- Technical test/script.js
+++ Technical test/script.js
@@ -2,8 +2,14 @@
 
 console.log("Hello There")
 
-window.onload =  async function(){
-    let response = await fetch("");
-    const data = await response.json();
-    console.log(data)
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoidGlsYWRlaHVuIiwiYSI6ImNrdXdhNzczaDBlN2sydW1yYTQwMzM5MHUifQ.n9w_c_FBhfcJOR-QADQcpA';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+// window.onload =  async function(){
+//     let response = await fetch("");
+//     const data = await response.json();
+//     console.log(data)
+// }
