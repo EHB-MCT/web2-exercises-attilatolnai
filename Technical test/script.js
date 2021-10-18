3c61a9c588cde0f7381dbe101ed958b090eb17c9
attilatolnai <attila.tolnai@student.ehb.be>
2021-10-18 09:19:14 +0200

    Map on location
    The map zooms in on your location when starting the website

--- Technical test/script.js
+++ Technical test/script.js
@@ -3,10 +3,38 @@
 console.log("Hello There")
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidGlsYWRlaHVuIiwiYSI6ImNrdXdhNzczaDBlN2sydW1yYTQwMzM5MHUifQ.n9w_c_FBhfcJOR-QADQcpA';
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11'
-});
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, 
+    { enableHighAccuracy: true
+})
+
+function successLocation(position){
+    console.log(position)
+    //console.log(position.coords.longitude)
+    //console.log(position.coords.latitude)
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation(){
+    
+}
+
+function setupMap(center){
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 16
+    });
+}
+
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v11'
+//     // center: center,
+//     // zoom: 20
+// });
+
 
 // window.onload =  async function(){
 //     let response = await fetch("");
